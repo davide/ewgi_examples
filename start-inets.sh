70332e32d6fb7b0e1032135ef13808bada4c57ec
davide <nesrait@gmail.com>
2009-09-02 16:08:13 +0100

    Added the dependencies lookup path in start-inets.sh.

--- start-inets.sh
+++ start-inets.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd `dirname $0`
-exec erl -pa $PWD/ebin -eval 'application:start(crypto)' -eval 'application:start(inets)' \
+exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -eval 'application:start(crypto)' -eval 'application:start(inets)' \
 -eval 'application:set_env(ewgi, app_module, ewgi_examples_web)' \
 -eval 'application:set_env(ewgi, app_function, dispatcher)' \
 -eval 'inets:start(httpd, [{server_name, "ewgi"}, {server_root, "."}, {document_root, "."}, {modules, [ewgi_inets]}, {port, 8000}])'
