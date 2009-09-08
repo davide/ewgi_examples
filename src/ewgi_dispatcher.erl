8aeb83df314ebabe7098c7b468c17690f5d7b86c
davide <nesrait@gmail.com>
2009-09-08 17:00:04 +0100

    Moved the session middleware example code from ewgi_dispatcher to the ewgi_session module.

--- src/ewgi_dispatcher.erl
+++ src/ewgi_dispatcher.erl
@@ -9,11 +9,6 @@
 %% Middleware exports
 -export([run/2, dispatch/1]).
 
--define(INCLUDE_IP, true).
--define(SESSION_TIMEOUT, 15 * 60 * 1000). %% 15 minutes
-
--define(SESSION_SERVER_REF, ewgi_session_server).
-
 %% This is the first middleware that gets called by the various webservers
 run(Ctx, []) ->
     dispatch(ewgi_api:path_info(Ctx), Ctx).
@@ -34,76 +29,16 @@ dispatch("/test.txt", Ctx) ->
 dispatch("/gzhello", Ctx) ->
     ewgi_deflate:run(ewgi_hello:run(Ctx,[]), []);
 
-
 dispatch("/session/cookie", Ctx) ->
-    {NoSession, Session, MiddlewareError} = session_create_apps("cookie"),
-    SessionStore = {ewgi_session_cookie_store, ["cookie_session_id", <<"ABCDEFGHIJKLMNOP">>, [{secure, false}]]},
-    ewgi_session:run(Ctx, [MiddlewareError, NoSession, Session, ?INCLUDE_IP, ?SESSION_TIMEOUT, SessionStore]);
+    ewgi_session:cookie_store_example(Ctx, create);
 dispatch("/session/cookie/delete", Ctx) ->
-    {NoSession, Session, MiddlewareError} = session_delete_apps(),
-    SessionStore = {ewgi_session_cookie_store, ["cookie_session_id", <<"ABCDEFGHIJKLMNOP">>, [{secure, false}]]},
-    ewgi_session:run(Ctx, [MiddlewareError, NoSession, Session, ?INCLUDE_IP, ?SESSION_TIMEOUT, SessionStore]);
-
+    ewgi_session:cookie_store_example(Ctx, delete);
 
 dispatch("/session/server", Ctx) ->
-    {NoSession, Session, MiddlewareError} = session_create_apps("server"),
-    SessionStore = {ewgi_session_server_store, [?SESSION_SERVER_REF, "server_session_id", false]},
-    ewgi_session:run(Ctx, [MiddlewareError, NoSession, Session, ?INCLUDE_IP, ?SESSION_TIMEOUT, SessionStore]);
+    ewgi_session:server_store_example(Ctx, create);
 dispatch("/session/server/delete", Ctx) ->
-    {NoSession, Session, MiddlewareError} = session_delete_apps(),
-    SessionStore = {ewgi_session_server_store, [?SESSION_SERVER_REF, "server_session_id", false]},
-    ewgi_session:run(Ctx, [MiddlewareError, NoSession, Session, ?INCLUDE_IP, ?SESSION_TIMEOUT, SessionStore]);
+    ewgi_session:server_store_example(Ctx, delete);
 
 dispatch(_, Ctx) ->   
     ewgi_api:response_message_body("404 Not Found", 
                                    ewgi_api:response_status({404, "Not Found"}, Ctx)).
-
-%% Private functions
-session_create_apps(Store) ->
-    NoSessionApp =
-	fun(Ctx) ->
-		Ctx1 = ewgi_session:new_session(Ctx),
-		ewgi_api:response_message_body("Created session! (please refresh the page)",
-					       ewgi_api:response_status({200, "OK"}, Ctx1))
-	end, 
-    SessionApp =
-	fun(Ctx) ->
-		case ewgi_session:get_session_data(Ctx, name) of
-		    undefined ->
-			Name = "Bill",
-			Body = ["Hello stranger! I'll call you ", Name, " from now on! (please refresh the page)"],
-			Ctx1 = ewgi_session:set_session_data(Ctx, name, "Bill");
-		    Name ->
-			Body = ["Hello ", Name, "! Nice to see you again! (no point in reloading again, delete the session by visiting /session/", Store, "/delete)"],
-			Ctx1 = Ctx
-		end,
-		Headers = [{"Content-type", "text/plain"}] ++ ewgi_api:response_headers(Ctx1),
-		Ctx2 = ewgi_api:response_headers(Headers, Ctx1),
-		Ctx3 = ewgi_api:response_status({200, "OK"}, Ctx2),
-		ewgi_api:response_message_body(Body, Ctx3)
-	end,
-    MiddlewareErrorApp = fun middleware_error/1,
-    {NoSessionApp, SessionApp, MiddlewareErrorApp}.
-
-session_delete_apps() ->
-    NoSessionApp =
-	fun(Ctx) ->
-		ewgi_api:response_message_body("No session to delete!",
-					       ewgi_api:response_status({200, "OK"}, Ctx))
-	end, 
-    SessionApp =
-	fun(Ctx) ->
-		Ctx1 = ewgi_session:delete_session(Ctx),
-		ewgi_api:response_message_body("Deleted session!",
-					       ewgi_api:response_status({200, "OK"}, Ctx1))
-	end,
-    MiddlewareErrorApp = fun middleware_error/1,
-    {NoSessionApp, SessionApp, MiddlewareErrorApp}.
-
-middleware_error(Ctx) ->
-    case ewgi_api:response_error(Ctx) of
-	Other ->
-	    error_logger:error_report(Other)
-    end,
-    ewgi_api:response_error(undefined, Ctx).
-
--- src/ewgi_session.erl
+++ src/ewgi_session.erl
@@ -24,6 +24,9 @@
 -export([run/2]).
 -export([new_session/1, get_session_data/1, get_session_data/2, set_session_data/3, remove_session_data/2, delete_session/1]).
 
+%% Examples
+-export([cookie_store_example/2, server_store_example/2]).
+
 -include("smak.hrl").
 -include("session.hrl").
 
@@ -210,3 +213,83 @@ save_session(Ctx) ->
 		    Ctx1
 	    end
     end.
+
+%%====================================================================
+%% example functions on how to use the session middleware
+%%====================================================================
+-define(INCLUDE_IP, true).
+-define(SESSION_TIMEOUT, 15 * 60 * 1000). %% 15 minutes
+
+-define(COOKIE_SIGNING_KEY, <<"ABCDEFGHIJKLMNOP">>).
+
+%% The server reference (Pid, LocalName, {Node,Name}, {global,Name})
+-define(SESSION_SERVER_REF, ewgi_session_server).
+
+cookie_store_example(Ctx, create) ->
+    {NoSession, Session, MiddlewareError} = session_create_apps("cookie"),
+    SessionStore = {ewgi_session_cookie_store, ["cookie_session_id", ?COOKIE_SIGNING_KEY, [{secure, false}]]},
+    ewgi_session:run(Ctx, [MiddlewareError, NoSession, Session, ?INCLUDE_IP, ?SESSION_TIMEOUT, SessionStore]);
+cookie_store_example(Ctx, delete) ->
+    {NoSession, Session, MiddlewareError} = session_delete_apps(),
+    SessionStore = {ewgi_session_cookie_store, ["cookie_session_id", ?COOKIE_SIGNING_KEY, [{secure, false}]]},
+    ewgi_session:run(Ctx, [MiddlewareError, NoSession, Session, ?INCLUDE_IP, ?SESSION_TIMEOUT, SessionStore]).
+
+server_store_example(Ctx, create) ->
+    {NoSession, Session, MiddlewareError} = session_create_apps("server"),
+    SessionStore = {ewgi_session_server_store, [?SESSION_SERVER_REF, "server_session_id", false]},
+    ewgi_session:run(Ctx, [MiddlewareError, NoSession, Session, ?INCLUDE_IP, ?SESSION_TIMEOUT, SessionStore]);
+server_store_example(Ctx, delete) ->
+    {NoSession, Session, MiddlewareError} = session_delete_apps(),
+    SessionStore = {ewgi_session_server_store, [?SESSION_SERVER_REF, "server_session_id", false]},
+    ewgi_session:run(Ctx, [MiddlewareError, NoSession, Session, ?INCLUDE_IP, ?SESSION_TIMEOUT, SessionStore]).
+    
+
+session_create_apps(Store) ->
+    NoSessionApp =
+	fun(Ctx) ->
+		Ctx1 = ewgi_session:new_session(Ctx),
+		ewgi_api:response_message_body("Created session! (please refresh the page)",
+					       ewgi_api:response_status({200, "OK"}, Ctx1))
+	end, 
+    SessionApp =
+	fun(Ctx) ->
+		case ewgi_session:get_session_data(Ctx, name) of
+		    undefined ->
+			Name = "Bill",
+			Body = ["Hello stranger! I'll call you ", Name, " from now on! (please refresh the page)"],
+			Ctx1 = ewgi_session:set_session_data(Ctx, name, "Bill");
+		    Name ->
+			Body = ["Hello ", Name, "! Nice to see you again! (no point in reloading again, delete the session by visiting /session/", Store, "/delete)"],
+			Ctx1 = Ctx
+		end,
+		Headers = [{"Content-type", "text/plain"}] ++ ewgi_api:response_headers(Ctx1),
+		Ctx2 = ewgi_api:response_headers(Headers, Ctx1),
+		Ctx3 = ewgi_api:response_status({200, "OK"}, Ctx2),
+		ewgi_api:response_message_body(Body, Ctx3)
+	end,
+    MiddlewareErrorApp = fun middleware_error/1,
+    {NoSessionApp, SessionApp, MiddlewareErrorApp}.
+
+session_delete_apps() ->
+    NoSessionApp =
+	fun(Ctx) ->
+		ewgi_api:response_message_body("No session to delete!",
+					       ewgi_api:response_status({200, "OK"}, Ctx))
+	end, 
+    SessionApp =
+	fun(Ctx) ->
+		Ctx1 = ewgi_session:delete_session(Ctx),
+		ewgi_api:response_message_body("Deleted session!",
+					       ewgi_api:response_status({200, "OK"}, Ctx1))
+	end,
+    MiddlewareErrorApp = fun middleware_error/1,
+    {NoSessionApp, SessionApp, MiddlewareErrorApp}.
+
+middleware_error(Ctx) ->
+    case ewgi_api:response_error(Ctx) of
+	Other ->
+	    error_logger:error_report(Other)
+    end,
+    ewgi_api:response_error(undefined, Ctx).
+
+
